2ee4aa25fb40536aa672ef5cec9316bcf83daf44
Christian <chriamue@gmail.com>
2017-10-03 10:29:21 +0200

    added android 23

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,5 @@ echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
 export HOME=/home/user
 chown $USER_ID:$USER_ID $HOME
-echo y | android update sdk --no-ui --all --filter "${ANDROID_COMPONENTS}"
-echo y | android update sdk --no-ui --all --filter "${GOOGLE_COMPONENTS}"
 
 exec /usr/local/bin/gosu user "$@"
